aa6ddf94a5efea32c8c36f15c809f46924299ada
WebShaper <batya.varfeysa.96@list.ru>
2022-09-19 15:26:22 +0300

    comicsList and update useHttp

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,8 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -17,20 +19,22 @@ const App = () => {
 
     return (
         <div className="app">
-            <AppHeader />
+            <AppHeader/>
             <main>
-                <ErrorBoundary>
-                    <RandomChar />
+                {/* <ErrorBoundary>
+                    <RandomChar/>
                 </ErrorBoundary>
-                {/* <div className="char__content">
+                <div className="char__content">
                     <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
+                        <CharList onCharSelected={onCharSelected}/>
                     </ErrorBoundary>
                     <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
+                        <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
-                </div> */}
-                <img className="bg-decoration" src={decoration} alt="vision" />
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/> */}
+                <AppBanner/>
+                <ComicsList/>
             </main>
         </div>
     )
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,7 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
     const [char, setChar] = useState({});
 
-    const {loading, error, getCharacter} = useMarvelService();
+    const { loading, error, getCharacter, clearError } = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -26,8 +26,9 @@ const RandomChar = () => {
     }
 
     const updateChar = () => {
+        clearError();
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-            getCharacter(id)
+        getCharacter(id)
             .then(onCharLoaded)
     }
     const errorMessage = error ? <ErrorMessage /> : null;
